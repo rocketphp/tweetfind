1ed04fff41466fbb60babc8e6644131cfce20b43
Eric Mugerwa <eric@rocketphp.io>
2015-09-06 21:55:58 +0100

    Add config.rb

---
+++ config.rb
@@ -0,0 +1,6 @@
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "public/html/css"
+sass_dir = "src/assets/sass"
+images_dir = "public/html/img"
+javascripts_dir = "public/html/js"
